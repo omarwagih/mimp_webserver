0103b34d0aeafb6ffbed1eacc221af2422919ae4
Omar Wagih <omarwagih@gmail.com>
2014-05-17 22:58:41 +0100

    Added pdf manual

--- public/R/generate_data/build_data.R
+++ public/R/generate_data/build_data.R
@@ -1,38 +1,11 @@
 require(parallel)
 require(seqinr)
 
-VERSION = '1.0'
 
-setwd('~/Development/mimp/public/R/generate_data/')
+setwd('~/Development/mimp_webserver/public/R/generate_data/')
 source('build_func.R')
-source('../mimp/R/Rmimp.R')
+source('~/Development/mimp/R/Rmimp.R')
 
-detachPackage <- function(pkg){
-  pkg = sprintf("package:%s", pkg)
-  res = tryCatch({
-    detach(pkg, unload=TRUE, character.only=T, force=T)
-    TRUE
-  }, error = function(e) {
-    FALSE
-  })
-  return(res)
-}
-
-build_package <- function(){
-  require(devtools)
-  # Compile things
-  document('../mimp/')
-  system('R CMD BUILD ../mimp/')
-  targz = sprintf('MIMP_%s.tar.gz', VERSION)
-  # Move up one directory
-  newf = file.path('..', targz)
-  re = file.rename(targz, newf)
-  # Install package
-  system(sprintf('R CMD INSTALL %s', newf))
-  # Reload in current environment
-  detachPackage('MIMP')
-  require(MIMP)
-}
 stop('x')
 print(load('neg.ps.data.10000.rsav'))
 print(load('pp.data.new.rsav'))
@@ -142,8 +115,6 @@ dens = sapply(names(dist), function(name){
 })
 
 
-# Build package
-build_package()
 
 stop('Done')
 
